7cd8b0d694792b85afd5d54c193bbe2c5ae6d6cb
amaroelias <amaronascimento2016@hotmail.com>
2026-05-06 13:20:50 -0300

    feat: shadcn/ui configurado no projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { Button } from "@/components/ui/button"
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Clique aqui</Button>
+    </>
+  );
 }
 
 export default App;
